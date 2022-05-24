680eb02a7a837ba06f938a9d1b5118558a78b601
Todd Trulock <toddstrulock@gmail.com>
2022-05-24 12:08:08 -0400

    fixed editing errors.

--- public/js/home.js
+++ public/js/home.js
@@ -1,23 +1,23 @@
-// everything checks out below exact same
+// // everything checks out below exact same
 
-const blogSection = document.querySelector('.blogs-section');
+// const blogSection = document.querySelector('.blogs-section');
 
-db.collection("blogs").get().then((blogs) => {
-    blogs.forEach(blog => {
-        if(blog.id != decodeURI(location.pathname.split("/").pop())){
-            createBlog(blog);
-        }
-    })
-})
+// db.collection("blogs").get().then((blogs) => {
+//     blogs.forEach(blog => {
+//         if(blog.id != decodeURI(location.pathname.split("/").pop())){
+//             createBlog(blog);
+//         }
+//     })
+// })
 
-const createBlog = (blog) => {
-    let data = blog.data();
-    blogSection.innerHTML += `
-    <div class="blog-card">
-        <img src="${data.bannerImage}" class="blog-image" alt="">
-        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
-        <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
-        <a href="/${blog.id}" class="btn dark">read</a>
-    </div>
-    `;
-}
+// const createBlog = (blog) => {
+//     let data = blog.data();
+//     blogSection.innerHTML += `
+//     <div class="blog-card">
+//         <img src="${data.bannerImage}" class="blog-image" alt="">
+//         <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
+//         <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+//         <a href="/${blog.id}" class="btn dark">read</a>
+//     </div>
+//     `;
+// }
